fff06a7c13d517f1ef166fb4fa488509f7a91389
Gopal Jadhav <gopaljadhav4657@gmail.com>
2024-05-29 15:48:37 +0530

    added day 1 practice with remaining parts

--- src/main/java/org/example/Day_1.java
+++ src/main/java/org/example/Day_1.java
@@ -3,7 +3,9 @@ package org.example;
 public class Day_1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello Gopal");
+
         System.out.println("I am a Automation Tester");
+        System.out.println("I have 4 year experience ");
+
     }
 }
