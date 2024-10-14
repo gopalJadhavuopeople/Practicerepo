790acfd1ea7447fbb8050854c8d5e74a6622188b
Gopal Jadhav <gopal.jadhav@uopeople.edu>
2024-10-14 12:49:53 +0530

    Update practiceDay3.java
    changes made in master branch

--- src/main/java/org/example/practiceDay3.java
+++ src/main/java/org/example/practiceDay3.java
@@ -4,5 +4,7 @@ public class practiceDay3 {
 
     public static void main(String[] args) {
         System.out.println("this is my 3rd class");
+        System.out.println("changes made in master branch");
     }
+
 }
