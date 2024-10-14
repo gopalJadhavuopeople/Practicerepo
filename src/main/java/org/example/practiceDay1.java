15ee7239aa4006ef4284bb5eb808ba0f643d9012
Gopal Jadhav <gopal.jadhav@uopeople.edu>
2024-10-14 11:41:16 +0530

    Update practiceDay1.java
    this is my second commit

--- src/main/java/org/example/practiceDay1.java
+++ src/main/java/org/example/practiceDay1.java
@@ -4,5 +4,8 @@ public class practiceDay1 {
 
     public static void main(String[] args) {
         System.out.println("this is day 1 code");
+
+        System.out.println("Add this in 2nd commit");
     }
+
 }
